6f919e937ea3a33263da206d7f6751cdd89b6c71
Ana Carolina Arellano <pg20@vfs.com>
2021-04-20 13:54:09 -0500

    added points for ladder

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,15 +1,21 @@
 // Copyright (c) 2021, Ana Carolina Arellano
 'use strict';
 
-import Game from './Game.js';
+import Game from './game/Game.js';
 
 //Main appplication
-
-//Application starts at chosen level
-$(".play").on(`click`, event => {
-    let game = new Game(); 
+$("#play").on(`click`, event => {
+    //Application starts at chosen level
+    console.log("Click en play")
+    let levelName = $("#level-list").children("option:selected").val()
+    console.log(levelName)
+    let game = new Game(levelName); 
     game.run();
 });
+        
+        
+        
+
 
 
 
